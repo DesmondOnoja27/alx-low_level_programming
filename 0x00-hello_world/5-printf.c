e1e7124d7f45fb5c13b66188d56664c440f7ccaf
DesmondOnoja27 <desmondskillz27@gmail.com>
2022-12-09 15:22:20 -0800

    four puts.c

--- 0x00-hello_world/5-printf.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-/**
-* main - print the string in the put function
-* Description: using the main function
-* this program will print "programming is like building a multilingual puzzle
