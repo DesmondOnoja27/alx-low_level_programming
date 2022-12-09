8fabfa815e0e42e1a4f34864a78d45fa266c5b69
DesmondOnoja27 <desmondskillz27@gmail.com>
2022-12-09 04:58:24 -0800

    four puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <studio.h>
 /**
 * main - print the string in the put function
-* Description : we will be making use of the main function
+* Description : we use the main function
 * this program prints "programming is like building a multilingual puzzle
-* Return : (0)
+* Return : 0
 */
 int main (void)
 {
