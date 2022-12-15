3b9522e1c5ed08d2e17d1d6147aafcd67d5ecec6
DesmondOnoja27 <desmondskillz27@gmail.com>
2022-12-14 19:24:58 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print alphabet_x10 - function to print abc ten times
+*
+* Return: 0
+*/
+void print_alphabet_x10(void)
+{
+char c;
+for (c = '0'; c <= '10'; _putchar(c = 'a'; c <= 'z'; c++))
+{
+_putchar('\n');
+}
+}
