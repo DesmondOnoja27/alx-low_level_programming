09946e676ee667e191c2e5098897125ad80c9657
DesmondOnoja27 <desmondskillz27@gmail.com>
2022-12-15 16:06:13 -0800

    lower c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _islower - checks if a character is lower case
+* c is the character
+* Return: 1 if letter is lower and 0 if it is not
+*/
+
+int _islower(int c)
+{
+
+if (c >= 'a'; && c <= 'z')
+return (1);
+else
+return (0);
+}
